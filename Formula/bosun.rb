02153b20666d1bf7dd01e2c4cffbdca26164ecc9
Andy Miller <rhuk@mac.com>
2026-05-18 17:28:47 -0600

    bosun 0.3.5: fix formula on Intel macOS
    Switch from nested on_intel/on_arm DSL inside on_macos/on_linux
    to the canonical GoReleaser-style if Hardware::CPU.intel? /
    if Hardware::CPU.arm? guards. The nested DSL fails to register a
    url at formula-load time on some Homebrew 5.x setups, breaking
    `brew info` / `brew outdated` with "formula requires at least a URL."
    
    Fixes yetidevworks/bosun#1.

--- Formula/bosun.rb
+++ Formula/bosun.rb
@@ -5,22 +5,22 @@ class Bosun < Formula
   version "0.3.5"
 
   on_macos do
-    on_arm do
+    if Hardware::CPU.arm?
       url "https://github.com/yetidevworks/bosun/releases/download/v0.3.5/bosun-darwin-aarch64.tar.gz"
       sha256 "91b14681cf53cc5dd1091ab80d49ad356c4d63127d8633d74229651e47bdf22d"
     end
-    on_intel do
+    if Hardware::CPU.intel?
       url "https://github.com/yetidevworks/bosun/releases/download/v0.3.5/bosun-darwin-x86_64.tar.gz"
       sha256 "6525619564e43af259ab6d7d5b14688b8c5afa3642e13be2d9e2960b4d1c0209"
     end
   end
 
   on_linux do
-    on_intel do
+    if Hardware::CPU.intel?
       url "https://github.com/yetidevworks/bosun/releases/download/v0.3.5/bosun-linux-x86_64.tar.gz"
       sha256 "432da32b100b76496a2525da4d97cfe8cba7a2fdccf4879504fa5c0fd80e81e2"
     end
-    on_arm do
+    if Hardware::CPU.arm?
       url "https://github.com/yetidevworks/bosun/releases/download/v0.3.5/bosun-linux-aarch64.tar.gz"
       sha256 "d7cd6f65998b104aa9483ac206f13f2d7b6532fb0ddec630da7ced2c4b60e04d"
     end
